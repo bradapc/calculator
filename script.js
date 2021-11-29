998291b89b51a3a7ac0e57f808e183eadbc87e57
Brad <brad.cocar@gmail.com>
2021-11-29 13:23:28 -0800

    Calculator displays numbers when pressed, computes basic results

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-function init(){
-    const digit = document.querySelectorAll('.digit');
-    digit.forEach(digitListener => digitListener.addEventListener('click', addDigit));
-}
+let operator;
+let num1 = '';
+let num2 = '';
+const digit = document.querySelectorAll('.digit');
+digit.forEach(digitListener => digitListener.addEventListener('click', addDigit));
+const operateSelect = document.querySelectorAll('.operator');
+operateSelect.forEach(operatorListener => operatorListener.addEventListener('click', setOperator));
+const equals = document.querySelector('.equals');
+equals.addEventListener('click', displayNumber);
 
 function add(a, b){
     return a + b;
@@ -24,7 +29,7 @@ function operate(operator, num1, num2) {
         return add(num1, num2);
     } else if(operator == '-') {
         return subtract(num1, num2);
-    } else if(operator == '*') {
+    } else if(operator == 'x') {
         return multiply(num1, num2);
     } else if(operator == '/') {
         return divide(num1, num2);
@@ -32,7 +37,31 @@ function operate(operator, num1, num2) {
 }
 
 function addDigit() {
-    alert(this.id);
+    const resultText = document.querySelector('.result-text');
+    let previousText = `${num1} ${operator}`
+    if(!operator) {
+        num1 += this.id;
+        resultText.textContent = num1;
+        return num1;
+    } else if(operator) {
+        num2 += this.id;
+        let newText = ` ${num2}`;
+        resultText.textContent = previousText + newText;
+        return num2;
+    }
 }
 
-init();
+function setOperator() {
+    if(!operator) {
+        operator = this.id;
+        const resultText = document.querySelector('.result-text');
+        resultText.textContent = `${num1} ${operator}`;
+    }
+}
+
+function displayNumber() {
+    num1 = Number.parseInt(num1);
+    num2 = Number.parseInt(num2);
+    let result = operate(operator, num1, num2);
+    alert(result);
+}
