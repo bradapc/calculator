4952d88c22878ce376f3fb685fa7d9384096ffc4
Brad <brad.cocar@gmail.com>
2021-11-29 14:38:48 -0800

    Added backspace

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let operator;
 let num1 = '';
 let num2 = '';
+let result = '';
 const digit = document.querySelectorAll('.digit');
 digit.forEach(digitListener => digitListener.addEventListener('click', addDigit));
 const operateSelect = document.querySelectorAll('.operator');
@@ -10,7 +11,6 @@ equals.addEventListener('click', displayNumber);
 const resultText = document.querySelector('.result-text');
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', clearInput);
-let result = '';
 
 function add(a, b){
     return a + b;
@@ -43,39 +43,66 @@ function operate(operator, num1, num2) {
 function addDigit() {
     let previousText = `${num1} ${operator}`
     if(!operator) {
-        num1 += this.id;
+        if(this.id == 'backspace' && num1) {
+            num1 = num1.slice(0, num1.length - 1);
+        } else {
+            if(this.id == '.' && !num1.includes('.')){
+                num1 += this.id;
+            } else if(this.id != '.' && this.id != 'backspace') {
+                num1 += this.id;
+            }
+    }
+    if(num1) {
         resultText.textContent = num1;
-        return num1;
+    } else {
+        resultText.textContent = '0';
+    }
     } else if(operator) {
-        num2 += this.id;
-        let newText = ` ${num2}`;
-        resultText.textContent = previousText + newText;
-        return num2;
+        if(this.id == 'backspace' && num2) {
+            num2 = num2.slice(0, num2.length - 1);
+        } else if(this.id == 'backspace'){
+            operator = '';
+            previousText = `${num1} ${operator}`
+        } else {
+            if(this.id == '.' && !num2.includes('.')) {
+                num2 += this.id;
+            } else if(this.id != '.') {
+                num2 += this.id;
+            }
+        }
+    let newText = ` ${num2}`;
+    resultText.textContent = previousText + newText;
     }
 }
 
 function setOperator() {
-    if(!operator) {
-        operator = this.id;
-        resultText.textContent = `${num1} ${operator}`;
-    } else if(operator && num1 && num2) {
-        operator = this.id;
-        resultText.textContent = `${result} ${operator}`;
-        displayNumber()
-    } else {
-        operator = this.id
-        resultText.textContent = `${result} ${operator}`
+    if(num1 && num1 != '-') {
+        if(!operator) {
+            operator = this.id;
+            resultText.textContent = `${num1} ${operator}`;
+        } else if(operator && num1 && num2) {
+            operator = this.id;
+            resultText.textContent = `${result} ${operator}`;
+            displayNumber()
+        } else {
+            operator = this.id
+            resultText.textContent = `${result} ${operator}`
+        }
+    } else if(!num1 && this.id == '-') {
+        num1 += this.id;
+        resultText.textContent = `${num1}`;
     }
 }
 
 function displayNumber() {
     if(operator && num1 && num2) {
-        num1 = Number.parseInt(num1);
-        num2 = Number.parseInt(num2);
+        num1 = Number.parseFloat(num1);
+        num2 = Number.parseFloat(num2);
         result = operate(operator, num1, num2);
         resultText.textContent = result;
-        num1 = result;
+        num1 = `${result}`;
         num2 = '';
+        operator = '';
     }
 }
 
